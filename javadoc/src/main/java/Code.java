55d6aa63184b4da98adbaa98ebcab9a7c562fe0e
황준호 <hwanghe159@naver.com>
2021-03-11 00:42:59 +0900

    javadoc

---
+++ javadoc/src/main/java/Code.java
@@ -0,0 +1,17 @@
+public class Code {
+    /**
+     * <pre> {@code
+     * public class JavadocTest {
+     *   // indentation and line breaks are kept
+     *
+     *  @literal @SuppressWarnings
+     *   public List<String> generics(){
+     *     // '<' and '>'  are displayed correctly
+     *     // '@' CANNOT be escaped with HTML code, though!
+     *   }
+     * }
+     * } </pre>
+     */
+    void codeTest() {
+    }
+}
