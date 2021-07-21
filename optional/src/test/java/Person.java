93d84af71fc5cdec5309e83ac21c659e08456dab
황준호 <42054054+hwanghe159@users.noreply.github.com>
2021-07-21 21:44:45 +0900

    spring batch, quartz 예제코드

---
+++ optional/src/test/java/Person.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class Person {
+
+  @Test
+  void name() {
+    Integer i1 = 127;
+    Integer i2 = 127;
+    Integer i3 = 128;
+    Integer i4 = 128;
+    System.out.println(i1);
+  }
+}
