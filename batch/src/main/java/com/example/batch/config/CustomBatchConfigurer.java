beed30d0d6d6aa8be1b5f4d751d7866e8866e71d
황준호 <42054054+hwanghe159@users.noreply.github.com>
2021-08-26 22:32:35 +0900

    예제 코드

---
+++ batch/src/main/java/com/example/batch/config/CustomBatchConfigurer.java
@@ -0,0 +1,49 @@
+package com.example.batch.config;
+
+import javax.sql.DataSource;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.support.DatabaseType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.PlatformTransactionManager;
+
+public class CustomBatchConfigurer extends DefaultBatchConfigurer {
+
+  @Autowired
+  @Qualifier("repositoryDataSource")
+  private DataSource dataSource;
+
+  @Autowired
+  @Qualifier("batchTransactionManager")
+  private PlatformTransactionManager transactionManager;
+
+  @Override
+  protected JobRepository createJobRepository() throws Exception {
+    JobRepositoryFactoryBean factoryBean = new JobRepositoryFactoryBean();
+    factoryBean.setDatabaseType(DatabaseType.MYSQL.getProductName());
+    factoryBean.setTablePrefix("FOO_");
+    factoryBean.setIsolationLevelForCreate("ISOLATION_REPEATABLE_READ");
+    factoryBean.setDataSource(this.dataSource);
+    factoryBean.afterPropertiesSet();
+    return factoryBean.getObject();
+  }
+
+  @Override
+  public PlatformTransactionManager getTransactionManager() {
+    return this.transactionManager;
+  }
+
+  @Override
+  protected JobExplorer createJobExplorer() throws Exception {
+    JobExplorerFactoryBean factoryBean = new JobExplorerFactoryBean();
+    factoryBean.setDataSource(this.dataSource);
+    factoryBean.setTablePrefix("FOO_");
+    factoryBean.afterPropertiesSet();
+    return factoryBean.getObject();
+  }
+
+}
