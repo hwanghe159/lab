bdd27fc66de659c7fd7336c54d07f402853c89b0
황준호 <42054054+hwanghe159@users.noreply.github.com>
2021-09-10 23:15:14 +0900

    quartz

---
+++ batch/src/main/java/com/example/batch/config/QuartzConfig.java
@@ -0,0 +1,32 @@
+package com.example.batch.config;
+
+import com.example.batch.job.BatchScheduledJob;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+
+  @Bean
+  public Trigger jobTrigger() {
+    SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+        .withIntervalInSeconds(5).withRepeatCount(4);
+
+    return TriggerBuilder.newTrigger()
+        .forJob(quartzJobDetail())
+        .withSchedule(scheduleBuilder)
+        .build();
+  }
+
+  @Bean
+  public JobDetail quartzJobDetail() {
+    return JobBuilder.newJob(BatchScheduledJob.class)
+        .storeDurably()
+        .build();
+  }
+}
