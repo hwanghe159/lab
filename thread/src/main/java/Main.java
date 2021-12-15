a568c34e7feefea6b0f4fb66cb4ea790b33e54f1
황준호 <42054054+hwanghe159@users.noreply.github.com>
2021-12-15 23:00:10 +0900

    execute, submit 차이

--- thread/src/main/java/Main.java
+++ thread/src/main/java/Main.java
@@ -1,37 +1,37 @@
-import java.time.LocalTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException {
-    ExecutorService executor = Executors.newFixedThreadPool(4);
+  public static void main(String[] args) {
+    // ExecutorService 인터페이스 구현객체 Executors 정적메서드를 통해 최대 스레드 개수가 2인 스레드 풀 생성
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-    executor.submit(() -> {
-      String threadName = Thread.currentThread().getName();
-      System.out.println("Job1 " + threadName);
-    });
-    executor.submit(() -> {
-      String threadName = Thread.currentThread().getName();
-      System.out.println("Job2 " + threadName);
-    });
-    executor.submit(() -> {
-      String threadName = Thread.currentThread().getName();
-      System.out.println("Job3 " + threadName);
-    });
-    executor.submit(() -> {
-      String threadName = Thread.currentThread().getName();
-      System.out.println("Job4 " + threadName);
-    });
+    for (int i = 0; i < 10; i++) {
+      Runnable runnable = () -> {
+        //스레드에게 시킬 작업 내용
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executorService;
+        int poolSize = threadPoolExecutor.getPoolSize();//스레드 풀 사이즈 얻기
+        String threadName = Thread.currentThread().getName();//스레드 풀에 있는 해당 스레드 이름 얻기
+        System.out.println("[총 스레드 개수:" + poolSize + "] 작업 스레드 이름: " + threadName);
+        //일부로 예외 발생 시킴
+        int value = Integer.parseInt("예외");
+      };
 
-    executor.shutdown();
-    if (executor.awaitTermination(20, TimeUnit.SECONDS)) {
-      System.out.println(LocalTime.now() + " All jobs are terminated");
-    } else {
-      System.out.println(LocalTime.now() + " some jobs are not terminated");
-      executor.shutdownNow();
+      //스레드풀에게 작업 처리 요청
+      executorService.execute(runnable);
+//      executorService.submit(runnable);
+
+      //콘솔 출력 시간을 주기 위해 메인스레드 0.01초 sleep을 걸어둠.
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
     }
-    System.out.println("end");
+    //스레드풀 종료
+    executorService.shutdown();
   }
 }
