ddc55439c490c8db0386f88c0977aa54fc1ec760
황준호 <42054054+hwanghe159@users.noreply.github.com>
2022-01-19 23:11:20 +0900

    kotlin enum

--- kotlin-basic/src/main/kotlin/HelloKotlin.kt
+++ kotlin-basic/src/main/kotlin/HelloKotlin.kt
@@ -1,3 +1,4 @@
+import Color.*
 
 fun main(args: Array<String>) {
     val persons = listOf(Person("영희", 24, true), Person("철수", age = 20, false))
@@ -15,3 +16,26 @@ fun main(args: Array<String>) {
 }
 
 fun getKorean(color: Color) =
+    when (color) {
+        RED -> "빨"
+        ORANGE -> "주"
+        YELLOW -> "노"
+        GREEN -> "초"
+        BLUE -> "파"
+        INDIGO -> "남"
+        VIOLET -> "보"
+    }
+
+fun getWarmth(color: Color) = when (color) {
+    RED, ORANGE, YELLOW -> "warm"
+    GREEN -> "neutral"
+    BLUE, INDIGO, VIOLET -> "cold"
+}
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
