983cc0d274f3e175a011565d10420589a2510bc0
황준호 <hwanghe159@naver.com>
2022-05-10 23:39:23 +0900

    Audience를 좀 더 자율적인 존재로
    - TicketSeller -> Bag 의존관계 제거
    - Audience.getBag 제거

--- book-object/src/main/java/domain/Audience.java
+++ book-object/src/main/java/domain/Audience.java
@@ -1,11 +1,20 @@
 package domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public class Audience {
 
   private Bag bag;
+
+  public Long buy(Ticket ticket) {
+    if (bag.hasInvitation()) {
+      bag.setTicket(ticket);
+      return 0L;
+    } else {
+      bag.setTicket(ticket);
+      bag.minusAmount(ticket.getFee());
+      return ticket.getFee();
+    }
+  }
 }
--- book-object/src/main/java/domain/TicketSeller.java
+++ book-object/src/main/java/domain/TicketSeller.java
@@ -8,14 +8,7 @@ public class TicketSeller {
   private TicketOffice ticketOffice;
 
   public void sellTo(Audience audience) {
-    if (audience.getBag().hasInvitation()) {
-      Ticket ticket = ticketOffice.getTicket();
-      audience.getBag().setTicket(ticket);
-    } else {
-      Ticket ticket = ticketOffice.getTicket();
-      audience.getBag().minusAmount(ticket.getFee());
-      ticketOffice.plusAmount(ticket.getFee());
-      audience.getBag().setTicket(ticket);
-    }
+    Long fee = audience.buy(ticketOffice.getTicket());
+    ticketOffice.plusAmount(fee);
   }
 }
