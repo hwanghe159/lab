8b562f0cbfa530f540faf88f41d5fd4d2d79ec5e
황준호 <hwanghe159@naver.com>
2022-05-10 23:30:16 +0900

    로직을 TicketSeller 내부로 캡슐화하여 Theater가 TicketSeller 안에 TicketOffice가 존재하는지 알 수 없게 된다 -> 결합도를 낮출 수 있음
    - Bag과의 의존관계 이동 (Theater -> TicketSeller)
    - Ticket과의 의존관계 이동 (Theater -> TicketSeller)
    - TicketSeller.getTicketOffice 제거

--- book-object/src/main/java/domain/TicketSeller.java
+++ book-object/src/main/java/domain/TicketSeller.java
@@ -1,11 +1,21 @@
 package domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public class TicketSeller {
 
   private TicketOffice ticketOffice;
+
+  public void sellTo(Audience audience) {
+    if (audience.getBag().hasInvitation()) {
+      Ticket ticket = ticketOffice.getTicket();
+      audience.getBag().setTicket(ticket);
+    } else {
+      Ticket ticket = ticketOffice.getTicket();
+      audience.getBag().minusAmount(ticket.getFee());
+      ticketOffice.plusAmount(ticket.getFee());
+      audience.getBag().setTicket(ticket);
+    }
+  }
 }
