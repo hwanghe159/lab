a77f11c2c899ffb61ff765fe8473d08f52ea68c9
황준호 <hwanghe159@naver.com>
2022-08-16 22:53:03 +0900

    Update IntegrationTester.java

--- book-object/movie-reservation/src/test/java/IntegrationTester.java
+++ book-object/movie-reservation/src/test/java/IntegrationTester.java
@@ -31,5 +31,12 @@ public class IntegrationTester {
             new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(13, 59))
         )
     );
+
+    Movie starWars = new Movie(
+        "스타워즈",
+        Duration.ofMinutes(210),
+        Money.wons(10000),
+        new NoneDiscountPolicy()
+    );
   }
 }
